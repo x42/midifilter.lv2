6cbba09b58851c90dd6604d7faa67f695de0aec9
Robin Gareus <robin@gareus.org>
2013-05-25 09:19:14 +0200

    add note-range filter

---
+++ filters/noterange.c
@@ -0,0 +1,42 @@
+MFD_FILTER(noterange)
+
+#ifdef MX_TTF
+
+	mflt:noterange
+	TTF_DEFAULTDEF("MIDI Note-Range Filter")
+	, TTF_IPORT(0, "channelf", "Filter Channel",  0.0, 16.0,  0.0, PORTENUMZ("Any"))
+	, TTF_IPORTINT(1, "lower", "Lowest Note",  0.0, 127.0,  0.0)
+	, TTF_IPORTINT(2, "upper", "Highest Note",  0.0, 127.0,  127.0)
+	.
+
+#elif defined MX_CODE
+
+static void filter_init_noterange(MidiFilter* self) { }
+
+static void
+filter_midi_noterange(MidiFilter* self,
+		uint32_t tme,
+		const uint8_t* const buffer,
+		uint32_t size)
+{
+	const uint8_t chs = midi_limit_chn(floor(*self->cfg[0]) -1);
+	const uint8_t chn = buffer[0] & 0x0f;
+	const uint8_t mst = buffer[0] & 0xf0;
+
+	if (size == 3
+			&& (mst == MIDI_NOTEON || mst == MIDI_NOTEOFF)
+			&& (floor(*self->cfg[0]) == 0 || chs == chn)
+		 )
+	{
+		const uint8_t low = midi_limit_val(floor(*self->cfg[1]));
+		const uint8_t upp = midi_limit_val(floor(*self->cfg[2]));
+		const uint8_t key = buffer[1] & 0x7f;
+		if (key >= low && key <= upp) {
+			forge_midimessage(self, tme, buffer, size);
+		}
+	} else {
+		forge_midimessage(self, tme, buffer, size);
+	}
+}
+
+#endif
