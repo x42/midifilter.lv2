373b7f2fbe5e814db33b2b33905d7f3f4ef64137
Robin Gareus <robin@gareus.org>
2013-05-25 04:19:47 +0200

    fix transpose filter: check for changed value every cycle -- not just on events

--- filters/miditranspose.c
+++ filters/miditranspose.c
@@ -11,6 +11,42 @@ MFD_FILTER(miditranspose)
 
 #elif defined MX_CODE
 
+static void filter_preproc_miditranspose(MidiFilter* self) {
+	if (floor(self->lcfg[1]) == floor(*self->cfg[1])) return;
+	const int transp = rint(*(self->cfg[1]));
+
+	int c,k;
+	uint8_t buf[3];
+	buf[2] = 0;
+	for (c=0; c < 16; ++c) {
+#if 0 /* send "all notes off" */
+		buf[0] = MIDI_CONTROLCHANGE | c;
+		buf[1] = 123;
+		forge_midimessage(self, tme, buf, 3);
+
+		// send "all sound off"
+		buf[0] = 0xb0 | i;
+		buf[1] = 120;
+		forge_midimessage(self, tme, buf, 3);
+#else /* re-transpose playing notes */
+		for (k=0; k < 127; ++k) {
+			if (!self->memCM[c][k]) continue;
+
+			buf[0] = MIDI_NOTEOFF | c;
+			buf[1] = midi_limit_val(k + self->memCI[c][k]);
+			buf[2] = 0;
+			forge_midimessage(self, 0, buf, 3);
+
+			buf[0] = MIDI_NOTEON | c;
+			buf[1] = midi_limit_val(k + transp);
+			buf[2] = self->memCM[c][k];
+			self->memCI[c][k] = transp;
+			forge_midimessage(self, 0, buf, 3);
+		}
+#endif
+	}
+}
+
 static void filter_init_miditranspose(MidiFilter* self) {
 	int c,k;
 
@@ -18,6 +54,7 @@ static void filter_init_miditranspose(MidiFilter* self) {
 		self->memCI[c][k] = -1000; // current key transpose
 		self->memCM[c][k] = 0; // velocity
 	}
+	self->preproc_fn = filter_preproc_miditranspose;
 }
 
 static void
@@ -28,43 +65,6 @@ filter_midi_miditranspose(MidiFilter* self,
 {
 	const int chs = midi_limit_chn(floor(*self->cfg[0]) -1);
 	const int transp = rint(*(self->cfg[1]));
-
-	/* config changed */
-	// TODO option what to do:
-	if (self->lcfg[1] != *(self->cfg[1])) {
-		int c,k;
-		uint8_t buf[3];
-		buf[2] = 0;
-		for (c=0; c < 16; ++c) {
-#if 0 /* send "all notes off" */
-			buf[0] = MIDI_CONTROLCHANGE | c;
-			buf[1] = 123;
-			forge_midimessage(self, tme, buf, 3);
-
-			// send "all sound off"
-			buf[0] = 0xb0 | i;
-			buf[1] = 120;
-			forge_midimessage(self, tme, buf, 3);
-#else /* re-transpose playing notes */
-			for (k=0; k < 127; ++k) {
-				if (!self->memCM[c][k]) continue;
-
-				buf[0] = MIDI_NOTEOFF | c;
-				buf[1] = midi_limit_val(k + self->memCI[c][k]);
-				buf[2] = 0;
-				forge_midimessage(self, tme, buf, 3);
-
-				buf[0] = MIDI_NOTEON | c;
-				buf[1] = midi_limit_val(k + transp);
-				buf[2] = self->memCM[c][k];
-				self->memCI[c][k] = transp;
-				forge_midimessage(self, tme, buf, 3);
-			}
-#endif
-		}
-	}
-	self->lcfg[1] = *(self->cfg[1]);
-
 
 	const uint8_t chn = buffer[0] & 0x0f;
 	const uint8_t key = buffer[1] & 0x7f;
