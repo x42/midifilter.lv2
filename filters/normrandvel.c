566d9d9d69eb7af57ba1e026522319f1b01658c6
Robin Gareus <robin@gareus.org>
2013-06-04 13:43:26 +0200

    NOOP - (pre-merge) code-indent

--- filters/normrandvel.c
+++ filters/normrandvel.c
@@ -14,8 +14,7 @@ MFD_FILTER(normrandvel)
 #elif defined MX_CODE
 
 static void filter_init_normrandvel(MidiFilter* self) {
-    srandom ((unsigned int) time (NULL));
-    self->memF[0] = 2*random() / (float)RAND_MAX - 1;
+	srandom ((unsigned int) time (NULL));
 }
 
 
@@ -24,67 +23,44 @@ static void filter_init_normrandvel(MidiFilter* self) {
 // User sets standard dev.
 static void
 filter_midi_normrandvel(MidiFilter* self,
-        uint32_t tme,
-        const uint8_t* const buffer,
-        uint32_t size)
+		uint32_t tme,
+		const uint8_t* const buffer,
+		uint32_t size)
 {
-    const int chs = midi_limit_chn(floor(*self->cfg[0]) -1);
-    const int chn = buffer[0] & 0x0f;
-    int mst = buffer[0] & 0xf0;
+	const int chs = midi_limit_chn(floor(*self->cfg[0]) -1);
+	const int chn = buffer[0] & 0x0f;
+	int mst = buffer[0] & 0xf0;
 
-    if (!size == 3
-            || !(mst == MIDI_NOTEON || (mst == MIDI_NOTEOFF))
-            || !(floor(*self->cfg[0]) == 0 || chs == chn)
-            )
-    {
-        forge_midimessage(self, tme, buffer, size);
-        return;
-    }
+	if (size != 3
+			|| !(mst == MIDI_NOTEON || (mst == MIDI_NOTEOFF))
+			|| !(floor(*self->cfg[0]) == 0 || chs == chn)
+			)
+	{
+		forge_midimessage(self, tme, buffer, size);
+		return;
+	}
 
-    const uint8_t vel = buffer[2] & 0x7f;
+	const uint8_t vel = buffer[2] & 0x7f;
 
-    if (mst == MIDI_NOTEON && vel ==0 ) {
-        mst = MIDI_NOTEOFF;
-    }
+	if (mst == MIDI_NOTEON && vel ==0 ) {
+		mst = MIDI_NOTEOFF;
+	}
 
-    uint8_t buf[3];
-    buf[0] = buffer[0];
-    buf[1] = buffer[1];
+	uint8_t buf[3];
+	buf[0] = buffer[0];
+	buf[1] = buffer[1];
 
-    const float dev = *(self->cfg[1]);
+	const float rnd = normrand(*(self->cfg[1]));
 
-    float U = 2.0* random() / (float)RAND_MAX - 1;//rand E(-1,1)
-    float S = SQUARE(U) + SQUARE(self->memF[0]);//map 2 random vars to unit circle
-
-    if(S>=1)//repull RV if outside unit circle
-    {
-        U = 2.0* random() / (float)RAND_MAX - 1;
-        S = SQUARE(U) + SQUARE(self->memF[0]);
-        if(S>=1)
-        {
-            U = 2.0* random() / (float)RAND_MAX - 1;
-            S = SQUARE(U) + SQUARE(self->memF[0]);
-            if(S>=1)
-            {//guarantee an exit, velocity will be unchanged
-                U=0;
-            }
-        }
-    }
-
-    self->memF[0] = U;//store RV for next round
-
-    const float rnd = U ? (dev*U*sqrt(-2*log(S)/S)) : 0;
-
-    switch (mst) {
-        case MIDI_NOTEON:
-            buf[2] = RAIL(rintf(buffer[2] + rnd), 1, 127);
-            break;
-        case MIDI_NOTEOFF:
-            buf[2] = RAIL(rintf(buffer[2] + rnd), 0, 127);
-            break;
-    }
-
-    forge_midimessage(self, tme, buf, size);
+	switch (mst) {
+		case MIDI_NOTEON:
+			buf[2] = RAIL(rintf(buffer[2] + rnd), 1, 127);
+			break;
+		case MIDI_NOTEOFF:
+			buf[2] = RAIL(rintf(buffer[2] + rnd), 0, 127);
+			break;
+	}
+	forge_midimessage(self, tme, buf, size);
 }
 
 #endif
