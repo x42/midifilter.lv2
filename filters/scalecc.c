fcc2738d35eb54a4f209164948cb2e40003ac5ba
Robin Gareus <robin@gareus.org>
2016-08-15 17:32:04 +0200

    consistent name (prefix MIDI)

--- filters/scalecc.c
+++ filters/scalecc.c
@@ -3,7 +3,7 @@ MFD_FILTER(scalecc)
 #ifdef MX_TTF
 
 	mflt:scalecc
-	TTF_DEFAULTDEF("Scale CC Value", "Scale CC Value")
+	TTF_DEFAULTDEF("MIDI Scale CC Value", "MIDI CC Scale")
 	, TTF_IPORT(0, "channelf", "Filter Channel", 0, 16, 0,
 			PORTENUMZ("Any")
 			DOC_CHANF)
