x42/midifilter.lv2

commit 45eb7ebf1205199a4e63ad76b40bcf44e7a1b8be

Add some chord presets

Makefile
@@ -91,7 +91,7 @@ filters.c: $(FILTERS)
 		done;
 	echo >> filters.c;
 
-$(BUILDDIR)manifest.ttl: manifest.ttl.in ttf.h filters.c
+$(BUILDDIR)manifest.ttl: manifest.ttl.in ttf.h filters.c presets/*.ttl
 	@mkdir -p $(BUILDDIR)
 	cat manifest.ttl.in > $(BUILDDIR)manifest.ttl
 	gcc -E -I. -DMX_MANIFEST filters.c \
@@ -106,7 +106,7 @@ ifneq ($(MOD),)
 		| uniq \
 		>> $(BUILDDIR)manifest.ttl
 endif
-	for file in presets/*.ttl; do head -n 3 $$file >> $(BUILDDIR)manifest.ttl; echo "rdfs:seeAlso <presets.ttl> ." >> $(BUILDDIR)manifest.ttl; done
+	for file in presets/*.ttl; do grep -A 3 "mfltpreset:" $$file | sed 's/ ;/;\n\trdfs:seeAlso <presets.ttl> ./g;s/^---*//g' >> $(BUILDDIR)manifest.ttl; done
 
 $(BUILDDIR)presets.ttl: presets.ttl.in presets/*.ttl
 	@mkdir -p $(BUILDDIR)

presets/chords.ttl
@@ -1,0 +1,39 @@
+mfltpreset:chord_major3
+	a pset:Preset;
+	lv2:appliesTo mfltplugin:midichord ;
+
+	rdfs:label "Triad" ;
+	lv2:port
+	[ lv2:symbol "c1";  pset:value 1.0 ],
+	[ lv2:symbol "c2";  pset:value 0.0 ],
+	[ lv2:symbol "c3";  pset:value 1.0 ],
+	[ lv2:symbol "c4";  pset:value 0.0 ],
+	[ lv2:symbol "c5";  pset:value 1.0 ],
+	[ lv2:symbol "c6";  pset:value 0.0 ],
+	[ lv2:symbol "c7";  pset:value 0.0 ],
+	[ lv2:symbol "c8";  pset:value 0.0 ],
+	[ lv2:symbol "c9";  pset:value 0.0 ],
+	[ lv2:symbol "c11"; pset:value 0.0 ],
+	[ lv2:symbol "c14"; pset:value 0.0 ],
+	[ lv2:symbol "_8";  pset:value 0.0 ]
+	.
+
+mfltpreset:chord_sus2
+	a pset:Preset;
+	lv2:appliesTo mfltplugin:midichord ;
+
+	rdfs:label "Sus2" ;
+	lv2:port
+	[ lv2:symbol "c3"; pset:value 0.0 ],
+	[ lv2:symbol "c2"; pset:value 1.0 ],
+	[ lv2:symbol "c4"; pset:value 0.0 ].
+
+mfltpreset:chord_sus4
+	a pset:Preset;
+	lv2:appliesTo mfltplugin:midichord ;
+
+	rdfs:label "Sus4" ;
+	lv2:port
+	[ lv2:symbol "c3"; pset:value 0.0 ],
+	[ lv2:symbol "c2"; pset:value 0.0 ],
+	[ lv2:symbol "c4"; pset:value 1.0 ].

presets/scalecc_invert.ttl
@@ -1,5 +1,5 @@
 mfltpreset:invertcc
-	a pset:Preset ;
+	a pset:Preset;
 	lv2:appliesTo mfltplugin:scalecc ;
 
 	rdfs:label "Invert CC Value" ;

commit 77a65b97273e8e5071fee160df266f9d9198ba44

Fix MacOS builds
"failed to expand CURIE `ntrdfs:seeAlso'" because '\n\t' is not
correctly expanded by OSX BSD sed.

This issue was introduced in the previous commit (45eb7ebf1)

Makefile
@@ -106,7 +106,7 @@ ifneq ($(MOD),)
 		| uniq \
 		>> $(BUILDDIR)manifest.ttl
 endif
-	for file in presets/*.ttl; do grep -A 3 "mfltpreset:" $$file | sed 's/ ;/;\n\trdfs:seeAlso <presets.ttl> ./g;s/^---*//g' >> $(BUILDDIR)manifest.ttl; done
+	for file in presets/*.ttl; do grep -A 3 "mfltpreset:" $$file | sed 's/ ;/; rdfs:seeAlso <presets.ttl> ./g;s/^---*//g' >> $(BUILDDIR)manifest.ttl; done
 
 $(BUILDDIR)presets.ttl: presets.ttl.in presets/*.ttl
 	@mkdir -p $(BUILDDIR)