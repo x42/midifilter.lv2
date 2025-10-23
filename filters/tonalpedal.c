4a4771fac23f8eaf1e9ce7ed83635d8e07bf7d56
Mark Knoop <mark@markknoop.com>
2025-10-23 08:28:15 +0100

    Adapt Tonal Pedal to emulate piano pedals
    Add option such that this filter can emulate the function of both the
    sustain and sostenuto piano pedals.
    
    Also correct the description of the Sostenuto filter.

--- filters/tonalpedal.c
+++ filters/tonalpedal.c
@@ -3,13 +3,17 @@ MFD_FILTER(tonalpedal)
 #ifdef MX_TTF
 
 	mflt:tonalpedal
-	TTF_DEFAULTDEF("MIDI Tonal Pedal", "MIDI Tonal Pedal")
+	TTF_DEFAULTDEF("MIDI Piano Pedal", "MIDI Piano Pedal")
 	, TTF_IPORT( 0, "pedal",  "Pedal CC", 0, 1, 0,
 			lv2:scalePoint [ rdfs:label "CC64" ; rdf:value 0 ] ;
 			lv2:scalePoint [ rdfs:label "CC66" ; rdf:value 1 ] ;
 			lv2:portProperty lv2:integer;  lv2:portProperty lv2:enumeration;)
 	, TTF_IPORTTOGGLE( 1, "forward_cc",   "Forward CC", 0)
-	; rdfs:comment "This filter holds any notes that are currently played when the sustain pedal is pressed for as long as the pedal remains pressed. Releasing the pedal sends Note-Off events. New notes played after presseing the pedal are not affected."
+	, TTF_IPORT( 2, "pedal_style",  "Pedal style", 0, 1, 1,
+			lv2:scalePoint [ rdfs:label "sustain" ; rdf:value 0 ] ;
+			lv2:scalePoint [ rdfs:label "sostenuto" ; rdf:value 1 ] ;
+			lv2:portProperty lv2:integer;  lv2:portProperty lv2:enumeration;)
+	; rdfs:comment "This filter holds any notes played when the pedal is pressed for as long as the pedal remains pressed. Releasing the pedal sends Note-Off events. When pedal style is set to 'sostenuto', new notes played after pressing the pedal are not affected."
 	.
 
 #elif defined MX_CODE
@@ -22,6 +26,7 @@ filter_midi_tonalpedal(MidiFilter* self,
 {
 	const int pedal = RAIL(*self->cfg[0], 0, 1);
 	const int fwdcc = RAIL(*self->cfg[1], 0, 1);
+	const int pedalstyle = RAIL(*self->cfg[2], 0, 1);
 
 	const uint8_t chn = buffer[0] & 0x0f;
 	const uint8_t key = buffer[1] & 0x7f;
@@ -61,9 +66,9 @@ filter_midi_tonalpedal(MidiFilter* self,
 	}
 
 	if (oldstate && !self->memI[chn]) {
-		/* pedal release, send note-off */
+		/* pedal release, send note-off unless manually held */
 		for (int k=0; k < 127; ++k) {
-			if (self->memCS[chn][k]) {
+			if (self->memCS[chn][k] && !self->memCM[chn][k]) {
 				uint8_t buf[3];
 				buf[0] = MIDI_NOTEOFF | chn;
 				buf[1] = k;
@@ -82,33 +87,35 @@ filter_midi_tonalpedal(MidiFilter* self,
 		return;
 	}
 
-	if (mst == MIDI_NOTEON && vel ==0 ) {
+	if (mst == MIDI_NOTEON && vel == 0) {
 		mst = MIDI_NOTEOFF;
 	}
+
+	/* record key state */
+	self->memCM[chn][key] = (mst == MIDI_NOTEON);
 
 	if (self->memI[chn]) {
 		/* pedal is pressed, filter note-off of held notes */
 		if (mst == MIDI_NOTEOFF && self->memCS[chn][key]) {
 			return;
 		}
-		forge_midimessage(self, tme, buffer, size);
-		return;
 	}
 
-	/* pedal is not pressed, collect potential notes */
-	if (mst == MIDI_NOTEON) {
-		self->memCS[chn][key] = 1;
-	} else if (mst == MIDI_NOTEOFF) {
-		self->memCS[chn][key] = 0;
+	/* record sustain state */
+	if ((pedalstyle && !self->memI[chn]) || !pedalstyle) {
+		self->memCS[chn][key] = (mst == MIDI_NOTEON);
 	}
+
 	forge_midimessage(self, tme, buffer, size);
+
 }
 
 static void filter_init_tonalpedal(MidiFilter* self) {
 	for (uint32_t c = 16; c < 16; ++c) {
 		self->memI[c] = 0; // per channel pedal (CC64)
 		for (int k=0; k < 127; ++k) {
-			self->memCS[c][k] = 0;
+			self->memCS[c][k] = 0; // notes sustained by pedal
+			self->memCM[c][k] = 0; // notes with key pressed
 		}
 	}
 }
